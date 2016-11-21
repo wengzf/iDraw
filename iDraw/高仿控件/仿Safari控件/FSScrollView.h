9edc0018baa939002fa78b0cabd116faee0c1300
wengzhifang <wengzhifang@yonglibao.com>
2016-11-21 19:46:08 +0800

    自定义scrollview

---
+++ iDraw/高仿控件/仿Safari控件/FSScrollView.h
@@ -0,0 +1,13 @@
+//
+//  FSScrollView.h
+//  iDraw
+//
+//  Created by 翁志方 on 2016/11/21.
+//  Copyright © 2016年 WZF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSScrollView : UIView
+
+@end
---
+++ iDraw/高仿控件/仿Safari控件/FSScrollView.m
@@ -0,0 +1,21 @@
+//
+//  FSScrollView.m
+//  iDraw
+//
+//  Created by 翁志方 on 2016/11/21.
+//  Copyright © 2016年 WZF. All rights reserved.
+//
+
+#import "FSScrollView.h"
+
+@implementation FSScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
