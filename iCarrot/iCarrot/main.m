d6484209c7ead596483bb8b5470f06ef0ab45529
wengzhifang <wengzhifang@yonglibao.com>
2017-01-23 17:54:16 +0800

    no message

---
+++ iCarrot/iCarrot/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iCarrot
+//
+//  Created by 翁志方 on 2017/1/22.
+//  Copyright © 2017年 wzf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
